1d1100d65b33891e9b33d8ec590f54cd7167d6e7
Mishu <mishudebnath927@gmail.com>
2026-04-13 22:02:24 +0600

    create a setup react router, app layout, deasign navbar and footer

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layout/RootLayout";
+import Home from "../pages/Home";
+import Timeline from "../pages/Timeline";
+import Stats from "../pages/Stats";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: RootLayout,
+        children: [
+            {
+                index: true,
+                Component: Home
+            },
+            {
+                path: '/timeline',
+                Component: Timeline
+            },
+            {
+                path: '/stats',
+                Component: Stats
+            }
+        ]
+    }
+]);
